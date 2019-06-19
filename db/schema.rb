10a41146be35cda16f3d8e360c5a6b3b89f1ac37
Przemek <p.pelszynski@selleo.com>
2019-06-19 14:26:49 +0200

    Added models, basic test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_164039) do
+ActiveRecord::Schema.define(version: 2019_06_19_115325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.string "phone_number"
+    t.string "status"
+    t.boolean "closed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_164039) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles", default: ["owner"], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
