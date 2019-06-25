5f19e889fdaf1e708e936039b15df76765e16a84
Przemek <p.pelszynski@selleo.com>
2019-06-25 15:34:16 +0200

    Added mail confirmations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,19 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDMAIL_USERNAME'],
+    password: ENV['SENDMAIL_PASSWORD'],
+    domain: 'localhost',
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Do not eager load code on boot.
   config.eager_load = false
