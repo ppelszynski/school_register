a48feee6c3d988b0cdd2458b07757860c2611b9e
Przemek <p.pelszynski@selleo.com>
2019-06-28 09:39:23 +0200

    Added confirmation helper, service

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,23 +1,23 @@
 class ConfirmationsController < ApplicationController
   before_action :get_teacher
 
-  def show
-    validate @teacher
-    email
-    redirect_to
+  def edit
+    authorize @teacher
   end
 
   def update
+    authorize @teacher
     @teacher.confirm!
   end
 
   private
 
   def get_teacher
+    binding.pry
     @teacher = User.find_by(email: email)
   end
 
   def email
-    DecryptEmail.call(params[:token])
+    DecryptEmail.call(params[:token]).result
   end
 end
